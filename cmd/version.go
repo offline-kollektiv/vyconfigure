1e7ea5391b8e7444360f8cdeea8cb1ce64f75f85
Peter Lehmann <xgwq@xnee.net>
2025-07-19 16:20:14 +0200

    🧑‍💻 get version from BuildInfo

--- cmd/version.go
+++ cmd/version.go
@@ -2,13 +2,14 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/urfave/cli/v3"
 )
 
 func version(ctx context.Context, cmd *cli.Command) error {
-	println(appVersion)
+	fmt.Println("vyconfigure", GetVersion())
 	os.Exit(0)
 	return nil
 }
