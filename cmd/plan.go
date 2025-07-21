6185138e504c826111d8f24897467867af7dabac
Peter Lehmann <xgwq@xnee.net>
2025-07-21 17:03:52 +0200

    🐍 switch to cobra
    https://github.com/spf13/cobra

--- cmd/plan.go
+++ cmd/plan.go
@@ -1,40 +1,52 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/fatih/color"
 	"github.com/offline-kollektiv/vyconfigure/pkg/api"
 	"github.com/offline-kollektiv/vyconfigure/pkg/diff"
 	"github.com/offline-kollektiv/vyconfigure/pkg/options"
-	"github.com/urfave/cli/v3"
+	"github.com/spf13/cobra"
 )
 
-func plan(ctx context.Context, cmd *cli.Command) error {
-	o := options.GetOptions(cmd)
+// planCmd represents the plan command
+var planCmd = &cobra.Command{
+	Use:     "plan <host> <api-key>",
+	Aliases: []string{"p"},
+	Short:   "Shows the diff between the current directory and VyOS instance",
+	Args:    cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		o := options.GetOptions(cmd, args)
 
-	client, err := api.CreateClient(o)
-	if err != nil {
-		return err
-	}
-
-	toDelete, toCreate, chg, err := diff.GetDiff(o, client)
-	if err != nil {
-		return err
-	}
-
-	if chg {
-		fmt.Println("Changes to be applied:")
-		for _, change := range toDelete {
-			color.Red("- delete " + change)
+		client, err := api.CreateClient(o)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
 		}
-		for _, change := range toCreate {
-			color.Green("+ set " + change)
-		}
-	} else {
-		fmt.Println("No changes to apply.")
-	}
 
-	return nil
+		toDelete, toCreate, chg, err := diff.GetDiff(o, client)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+
+		if chg {
+			fmt.Println("Changes to be applied:")
+			for _, change := range toDelete {
+				color.Red("- delete " + change)
+			}
+			for _, change := range toCreate {
+				color.Green("+ set " + change)
+			}
+		} else {
+			fmt.Println("No changes to apply.")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(planCmd)
 }
--- cmd/version.go
+++ cmd/version.go
@@ -1,15 +1,30 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
-	"os"
+	"runtime/debug"
 
-	"github.com/urfave/cli/v3"
+	"github.com/spf13/cobra"
 )
 
-func version(ctx context.Context, cmd *cli.Command) error {
-	fmt.Println("vyconfigure", cmd.Version)
-	os.Exit(0)
-	return nil
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("vyconfigure", GetVersion())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+func GetVersion() (ret string) {
+	if b, ok := debug.ReadBuildInfo(); ok && len(b.Main.Version) > 0 {
+		ret = b.Main.Version
+	} else {
+		ret = "unknown"
+	}
+	return
 }
