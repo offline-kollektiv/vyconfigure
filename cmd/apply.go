a471b7e366f6165fc8d28b988937e873ade560cf
timoschirmer <97628241+timoschirmer@users.noreply.github.com>
2025-07-21 13:01:45 +0200

    ♻️ refactor: extract shared logic from apply and plan into GetDiff (#29)

--- cmd/apply.go
+++ cmd/apply.go
@@ -5,53 +5,26 @@ import (
 	"fmt"
 
 	"github.com/offline-kollektiv/vyconfigure/pkg/api"
-	"github.com/offline-kollektiv/vyconfigure/pkg/config"
 	"github.com/offline-kollektiv/vyconfigure/pkg/convert"
+	"github.com/offline-kollektiv/vyconfigure/pkg/diff"
 	"github.com/offline-kollektiv/vyconfigure/pkg/options"
-	r3diff "github.com/r3labs/diff/v3"
 	"github.com/urfave/cli/v3"
 )
 
 func apply(ctx context.Context, cmd *cli.Command) error {
 	o := options.GetOptions(cmd)
 
-	// get remote config as cmds
 	client, err := api.CreateClient(o)
 	if err != nil {
 		return err
 	}
 
-	d, err := client.RetrieveJson()
+	toDelete, toCreate, chg, err := diff.GetDiff(o, client)
 	if err != nil {
 		return err
 	}
 
-	rc, _ := convert.JsonToCmds(d, "")
-
-	// get local config as cmds
-	lc, err := config.ReadAsCmds(o)
-	if err != nil {
-		return err
-	}
-
-	// get diff
-	changelog, err := r3diff.Diff(rc, lc)
-	if err != nil {
-		return err
-	}
-
-	var toDelete []string
-	var toCreate []string
-	if len(changelog) > 0 {
-		for _, change := range changelog {
-			if change.Type == "create" {
-				toCreate = append(toCreate, change.To.(string))
-			}
-			if change.Type == "delete" {
-				toDelete = append(toDelete, change.From.(string))
-			}
-		}
-	} else {
+	if !chg {
 		fmt.Println("No changes to apply.")
 		return nil
 	}
--- cmd/plan.go
+++ cmd/plan.go
@@ -6,50 +6,31 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/offline-kollektiv/vyconfigure/pkg/api"
-	"github.com/offline-kollektiv/vyconfigure/pkg/config"
-	"github.com/offline-kollektiv/vyconfigure/pkg/convert"
+	"github.com/offline-kollektiv/vyconfigure/pkg/diff"
 	"github.com/offline-kollektiv/vyconfigure/pkg/options"
-	diff "github.com/r3labs/diff/v3"
 	"github.com/urfave/cli/v3"
 )
 
 func plan(ctx context.Context, cmd *cli.Command) error {
 	o := options.GetOptions(cmd)
 
-	// get remote config as cmds
 	client, err := api.CreateClient(o)
 	if err != nil {
 		return err
 	}
 
-	d, err := client.RetrieveJson()
+	toDelete, toCreate, chg, err := diff.GetDiff(o, client)
 	if err != nil {
 		return err
 	}
 
-	rc, _ := convert.JsonToCmds(d, "")
-
-	// get local config as cmds
-	lc, err := config.ReadAsCmds(o)
-	if err != nil {
-		return err
-	}
-
-	// get diff
-	changelog, err := diff.Diff(rc, lc)
-	if err != nil {
-		return err
-	}
-
-	if len(changelog) > 0 {
+	if chg {
 		fmt.Println("Changes to be applied:")
-		for _, change := range changelog {
-			if change.Type == "create" {
-				color.Green("+ set " + change.To.(string))
-			}
-			if change.Type == "delete" {
-				color.Red("- delete " + change.From.(string))
-			}
+		for _, change := range toDelete {
+			color.Red("- delete " + change)
+		}
+		for _, change := range toCreate {
+			color.Green("+ set " + change)
 		}
 	} else {
 		fmt.Println("No changes to apply.")
